24dad0a9c8a7fabae4b2984a24c74f26840d57d6
margaiwangara <margaiwangara@gmail.com>
2021-05-18 12:52:14 +0300

    🔌 client: added socket.io-client for websocket implementation 🔌

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,2 +1,4 @@
+export const endpoint =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:2111' : '';
 export const gqlEndpoint =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:2111/graphql' : '';
+  process.env.NODE_ENV === 'development' ? `${endpoint}/graphql` : '';
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,22 +2,54 @@ import { Box } from '@chakra-ui/layout';
 import { withUrqlClient } from 'next-urql';
 import NavBar from '../components/NavBar';
 import Wrapper from '../components/Wrapper';
-import { usePostsQuery } from '../generated/graphql';
+import { useMeQuery, usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../lib/createUrqlClient';
+import SocketIOClient, { Socket } from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import { endpoint } from '../config';
+import { useToast } from '@chakra-ui/toast';
+import Loading from '../components/Loading';
+import { isServer } from '../utils/isServer';
+import { DefaultEventsMap } from 'socket.io-client/build/typed-events';
 
 function Home() {
   const [{ fetching, data }] = usePostsQuery();
+  const [{ fetching: currentUserFetching, data: currentUser }] = useMeQuery({
+    pause: isServer(),
+  });
+  const toast = useToast();
 
+  const [socket, setSocket] =
+    useState<Socket<DefaultEventsMap, DefaultEventsMap> | null>(null);
+
+  useEffect(() => {
+    const io = SocketIOClient(endpoint);
+    setSocket(io);
+
+    return () => {
+      io.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket?.emit('addUser', currentUser?.me?.id);
+    socket?.on('getUsers', (users) => {
+      console.log('users', users);
+    });
+  }, [currentUser?.me]);
   return (
-    <Box h="100vh" w="100%" overflowX="hidden">
-      <NavBar />
-      <Wrapper variant="regular">
-        {fetching && <p>Loading Posts...</p>}
-        {data?.posts.map((p) => (
-          <div key={p.id}>{p.title}</div>
-        ))}
-      </Wrapper>
-    </Box>
+    <>
+      {/* {currentUserFetching && <Loading />} */}
+      <Box h="100vh" w="100%" overflowX="hidden">
+        <NavBar />
+        <Wrapper variant="regular">
+          {fetching && <p>Loading Posts...</p>}
+          {data?.posts.map((p) => (
+            <div key={p.id}>{p.title}</div>
+          ))}
+        </Wrapper>
+      </Box>
+    </>
   );
 }
 
